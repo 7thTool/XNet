61171f052dbe34c1d8c171a4d5dcb03e6d9d7f5e
7thtool <i7thtool@qq.com>
2018-11-12 12:42:25 +0800

    init

---
+++ web/main.cpp
@@ -0,0 +1,19 @@
+#include "XNet"
+//using namespace XNet;
+
+class XServer : public XServerT<XServer>
+{
+
+};
+
+int main(int argc, char* argv[])
+{
+	XServer server;
+	server.start(5, 5);
+	//server.listen(std::atoi(argv[1]));
+	getchar();
+	server.stop();
+
+	return 0;
+}
+
